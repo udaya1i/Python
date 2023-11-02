0a81c1f2624fc19a4662b24ed02e9102325ad465
udaya <ping.udaya@gmail.com>
2023-11-02 15:37:10 +0545

    crud operation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 
 app = Flask(__name__)
     
@@ -13,21 +14,50 @@ class Todo(db.Model):
     title = db.Column(db.String(200), nullable =False)
     desc = db.Column(db.String(500), nullable = False)
     date_created =  db.Column(db.DateTime, default = datetime.utcnow)
-    
-    
+       
     def __repr__(self) -> str:
         return f'{self.sno} - {self.title}'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    todo = Todo(title = "abc", desc = 'alksdjfalksdjflakj')
-    db.session.add(todo)
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        if title and desc:
+            new_todo = Todo(title=title, desc = desc)
+            db.session.add(new_todo)
+            db.session.commit()
+            flash('Task added successfully','success')
+        else:
+            flash('Please fill the form','error')
+    lists = Todo.query.all()
+    return render_template('index.html', lists = lists)
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    all_todo = Todo.query.filter_by(sno = sno).first()
+    db.session.delete(all_todo)
     db.session.commit()
-    return render_template('index.html')
+    return redirect('/')
+
+# @app.route('update/<int:sno>')
+# def udpate(sno):
+#     all_todo = Todo.query.filter_by(sno= sno).first()
+#     all_todo['title'] = request.form['title']
+#     all_todo['desc'] = request.form['desc']
+#     if all_todo['title'] and all_todo['desc']:
+#         update_todo = Todo[all_todo['title']=all_todo['title'], all_todo['desc']=all_todo['desc']]
+        
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.secret_key = 'secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    # sess.init_app(app)
+    app.debug = True
+    app.run()
     
 
 
